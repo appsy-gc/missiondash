f31c4b13e9d8c7b0ccbdf24d36c85b51b513c585
Chris Apps <chrisapps@Chriss-Mac-mini.local>
2024-12-14 17:00:04 +1000

    Added CREATE status route and tested in insomnia

--- controllers/status_controller.py
+++ controllers/status_controller.py
@@ -28,7 +28,28 @@ def get_status(status_id):
         return {"message": f"Status with id: {status_id} does not exist"}, 404
 
 # Create - /statuses - POST
-
+@statuses_bp.route("/", methods=["POST"])
+def create_status():
+    try:
+        # Get information from request body
+        body_data = StatusSchema().load(request.get_json(), partial=True)
+        # Create status instance
+        new_status = Status(
+            name = body_data.get("name")
+        )
+        # Add new status and commit
+        db.session.add(new_status)
+        db.session.commit()
+        return StatusSchema().dump(new_status), 201
+    except IntegrityError as err:
+        print(err.orig.pgcode)
+        if err.orig.pgcode == errorcodes.NOT_NULL_VIOLATION:
+            # not_null_violoation
+            # Return specific field that is in violoation
+            return {"message": f"The field '{err.orig.diag.column_name}' is required"}, 409
+        if err.orig.pgcode == errorcodes.UNIQUE_VIOLATION:
+                # unique_constraint_violoation
+                return {"message": "Status name already exists"}, 409
 
 # Update - /statuses/id - PUT and PATCH
 
