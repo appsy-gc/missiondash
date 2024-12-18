950d474eb4499d0270f55950e454b0fc3f557467
Chris Apps <chrisapps@Chriss-Mac-mini.local>
2024-12-19 07:29:58 +1000

    completed crew_member schema

--- models/crew_member.py
+++ models/crew_member.py
@@ -1,18 +1,22 @@
 from init import db, ma
 from marshmallow import fields
-from marshmallow.validate import Length, Regexp
+from marshmallow.validate import Length, Regexp, OneOf
+
+VALID_ROLES = ("Pilot", "Commander", "Flight Engineer", "Navigator", "Weapons Specialist")
+VALID_AVAILABILITY = ("Available","On Mission", "On Leave", "Unavailable", "In Training", "Retired")
 
 class CrewMember(db.Model):
     __tablename__ = "crew_members"
 
     crew_member_id = db.Column(db.Integer, primary_key=True)
-    crew_id = db.Column(db.Integer, nullable=False)
+    crew_id = db.Column(db.Integer, db.ForeignKey("crews.crew_id"))
+    crew = db.relationship("Crew", back_populates="crew_members")
     name = db.Column(db.String(100), nullable=False, unique=True)
     role = db.Column(db.String(100), nullable=False)
     availability = db.Column(db.String, nullable=False)
 
 class CrewMemberSchema(ma.Schema):
-    # Validation for crew name
+    # Validation for crew member name
     name = fields.String(
         validate=[
         Length(min=6, error="Crew name must have more than 6 characters."),
@@ -23,6 +27,12 @@ class CrewMemberSchema(ma.Schema):
         ],
         required=True   
     )
+    # Validation for crew member role
+    role = fields.String(validate=OneOf(VALID_ROLES), required=True)
+    # Validation for crew member availability
+    availability = fields.String(validate=OneOf(VALID_AVAILABILITY), required=True)
+
+    crew = fields.List(fields.Nested("CrewSchema", only=["name"]))
      
     class Meta:
         fields = ("crew_member_id", "crew_id", "name", "role", "availability")
