9adf350ecd12fcc7083982ced158c50dc3f9a1a8
Chris Apps <chrisapps@Chriss-Mac-mini.local>
2024-12-19 18:04:12 +1000

    GET one route created for crew member controller

--- controllers/crew_member_controller.py
+++ controllers/crew_member_controller.py
@@ -8,10 +8,13 @@ from marshmallow.exceptions import ValidationError
 crew_members_bp = Blueprint("crew_members", __name__, url_prefix="/crew_members")
 
 # Function to get crew_member id
-
+def get_crew_member_id(crew_member_id):
+    stmt = db.select(CrewMember).filter_by(crew_member_id=crew_member_id)
+    return db.session.scalar(stmt)
 
 # Function to send crew member not found message
-
+def crew_member_not_found_message(crew_member_id):
+    return {"message": f"Crew member with id: {crew_member_id} does not exist"}, 404
 
 # Function to create or update information
 
@@ -19,13 +22,19 @@ crew_members_bp = Blueprint("crew_members", __name__, url_prefix="/crew_members"
 # Read all - /crew_members - GET
 @crew_members_bp.route("/", methods=["GET"])
 def get_crew_members():
-    crew_id = request.args.get("crew_id")
     stmt = db.select(CrewMember).order_by(CrewMember.crew_member_id)
     crew_members_list = db.session.scalars(stmt)
     return CrewMemberSchema(many=True).dump(crew_members_list)
 
 
 # Read one - /crew_members/id - GET
+@crew_members_bp.route("/<int:crew_member_id>", methods=["GET"])
+def get_crew_member(crew_member_id):
+    crew_member = get_crew_member_id(crew_member_id)
+    if crew_member:
+        return CrewMemberSchema().dump(crew_member)
+    else:
+        return crew_member_not_found_message(crew_member_id)
 
 
 # Create - /crew_members - POST
